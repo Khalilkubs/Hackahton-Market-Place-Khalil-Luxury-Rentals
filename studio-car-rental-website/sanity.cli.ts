423252ecec6c77b156c7ea82842eb5f806220c7a
Khalil-ur-Rehman <Khalil.kubs@gmail.com>
2025-02-16 00:27:35 +0500

    New  changes

--- studio-car-rental-website/sanity.cli.ts
+++ studio-car-rental-website/sanity.cli.ts
@@ -2,7 +2,7 @@ import {defineCliConfig} from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
-    projectId: 'qslspj41',
+    projectId: 'fa8s30m9',
     dataset: 'production'
   },
   /**
--- studio-car-rental-website/sanity.config.ts
+++ studio-car-rental-website/sanity.config.ts
@@ -1,14 +1,21 @@
-import {defineConfig} from 'sanity'
-import {structureTool} from 'sanity/structure'
-import {visionTool} from '@sanity/vision'
-import {schemaTypes} from './schemaTypes'
+import { defineConfig } from 'sanity'
+import { structureTool } from 'sanity/structure'
+import { visionTool } from '@sanity/vision'
+import { schemaTypes } from './schemaTypes'
+
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
+
+if (!projectId || !dataset) {
+  throw new Error('Missing Sanity environment variables')
+}
 
 export default defineConfig({
   name: 'default',
   title: 'car rental website',
 
-  projectId: 'qslspj41',
-  dataset: 'production',
+  projectId,
+  dataset,
 
   plugins: [structureTool(), visionTool()],
 
